abe9740ce9ab1eee58a15def2d84f990a7adc29e
Geraldfrank255 <geraldfrankndunguru@gmail.com>
2026-09-14 23:56:34 +0100

    Add login URL and redirect settings

--- core/settings.py
+++ core/settings.py
@@ -71,3 +71,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/admin/login/'
+LOGIN_REDIRECT_URL = '/'
