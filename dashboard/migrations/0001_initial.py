8efaaacb318e2cadbca97c0e3dfaa04b33325ffd
Geraldfrank255 <geraldfrankndunguru@gmail.com>
2026-09-14 23:37:38 +0100

    Create initial migration for CustomUser model

---
+++ dashboard/migrations/0001_initial.py
@@ -0,0 +1,43 @@
+import django.contrib.auth.models
+import django.contrib.auth.validators
+from django.db import migrations, models
+import django.utils.timezone
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ('auth', '0012_alter_user_first_name_max_length'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CustomUser',
+            fields=[
+                ('id', 'BigAutoField', auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('password', 'CharField', max_length=128, verbose_name='password')),
+                ('last_login', 'DateTimeField', blank=True, null=True, verbose_name='last login')),
+                ('is_superuser', 'BooleanField', default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('username', 'CharField', error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username')),
+                ('first_name', 'CharField', blank=True, max_length=150, verbose_name='first name')),
+                ('last_name', 'CharField', blank=True, max_length=150, verbose_name='last name')),
+                ('email', 'EmailField', blank=True, max_length=254, verbose_name='email address')),
+                ('is_staff', 'BooleanField', default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', 'BooleanField', default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', 'DateTimeField', default=django.utils.timezone.now, verbose_name='date joined')),
+                ('role', 'CharField', choices=[('admin', 'Admin'), ('salesman', 'Salesman')], default='salesman', max_length=10)),
+                ('business_assigned', 'CharField', blank=True, choices=[('mpesa', 'M-Pesa'), ('salon', 'Salon'), ('hotspot', 'Hotspot')], max_length=10, null=True)),
+                ('groups', 'ManyToManyField', blank=True, help_text='The groups this user belongs to.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
+                ('user_permissions', 'ManyToManyField', blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
+            ],
+            options={
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
+                'abstract': False,
+            },
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
+        ),
+    ]
